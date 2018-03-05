6eaef68bc72a68becb84934de99073d00dfc2a41
Eliran Choen <Ecahn@g.jct.ac.il>
2018-03-05 22:09:23 +0200

    just test

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -1,5 +1,7 @@
 package primitives;
 
+import java.util.Vector;
+
 public class Point3D extends Point2D {
 
     protected Coordinate _z;
@@ -10,9 +12,27 @@ public class Point3D extends Point2D {
         _z = new Coordinate(z);
     }
 
+    /**
+     * copy constructor
+     * @param other The Point3D that will be copied
+
+     */
     public Point3D(Point3D other){
         super(other);
         _z = new Coordinate(other._z);
 
+    }
+
+
+    // ***************** Getters/Setters ********************** //
+
+    public double get_z() {
+        return _z.get_cord();
+    }
+
+
+    // ***************** Administration  ******************** //
+    public Vector vectorSubstract(Point3D other){
+        return new Vector(get_x()-other.get_x(),get_y()-other.get_y(),get_z()-other.get_z());
     }
 }
