65d871bd449cbdf7164347e5ebd2139ecb1f3e3b
Eliran Choen <Ecahn@g.jct.ac.il>
2018-03-05 20:55:13 +0200

    just test

--- src/primitives/Coordinate.java
+++ src/primitives/Coordinate.java
@@ -1,6 +1,33 @@
 package primitives;
 
 public class Coordinate {
-    //test
-    ;
+    private double _cord;
+    private final double EPSILON = 0.000001;
+    // ***************** Constructors ********************** //
+    public Coordinate(double cord){
+        _cord = cord;
+    }
+
+
+    // ***************** Getters/Setters ********************** //
+    public double get_cord() {
+        return _cord;
+    }
+
+    // ***************** Administration  ******************** //
+
+    @Override
+    public String toString() {
+        return "" + get_cord();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Coordinate))
+            return false;
+        if (Math.abs(get_cord()-((Coordinate)((Coordinate) obj)).get_cord())<EPSILON)
+            return true;
+        else
+            return false;
+    }
 }
