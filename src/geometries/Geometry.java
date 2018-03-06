7b6c51179dda0754c836d3147ea757aa7ed0c6e1
Israel <Israel@github.com>
2018-03-06 12:47:54 +0200

    Geometries

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,4 +1,24 @@
 package geometries;
 
+import primitives.Point3D;
+import primitives.Vector;
+
 public abstract class Geometry {
+
+    // ***************** Constructors ********************** //
+
+    /**
+     * ctor
+     */
+    public Geometry(){}
+
+    /**
+     * Copy Ctor
+     * @param temp
+     */
+    public Geometry(Geometry temp) {
+
+    }
+    // ***************** Operations ******************** //
+    public abstract Vector getNormal(Point3D temp);
 }
--- src/geometries/RadialGeometry.java
+++ src/geometries/RadialGeometry.java
@@ -3,7 +3,19 @@ package geometries;
 public abstract class RadialGeometry extends Geometry {
     protected double _radius;
 
-
+    // ***************** Constructors ********************** //
+    public RadialGeometry(double rad){
+        super();
+        this._radius=rad;
+    }
+    /**
+     * Copy ctor
+     * @param temp
+     */
+    public RadialGeometry(RadialGeometry temp){
+        super(temp);
+        this._radius=temp.get_radius();
+    }
 
     // ***************** Getters/Setters ********************** //
 
