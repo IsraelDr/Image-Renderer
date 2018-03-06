99541bfb7124fb877401443225cadba43f3081b3
Israel <Israel@github.com>
2018-03-06 12:29:52 +0200

    Geometries

---
+++ src/geometries/RadialGeometry.java
@@ -0,0 +1,13 @@
+package geometries;
+
+public abstract class RadialGeometry extends Geometry {
+    protected double _radius;
+
+
+
+    // ***************** Getters/Setters ********************** //
+
+    public double get_radius() {
+        return _radius;
+    }
+}
