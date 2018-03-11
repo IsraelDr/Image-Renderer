8c5533655dafd60ccee22dda0735734e5c30a743
Israel <Israel@github.com>
2018-03-11 21:09:29 +0200

    Tests

---
+++ src/unittests/CoordinateTest.java
@@ -0,0 +1,24 @@
+package unittests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CoordinateTest {
+
+    @Test
+    public void get_cord() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void substract() {
+    }
+}
