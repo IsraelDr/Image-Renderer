36b70ff42bbc9c8abd542cb4552ea549e548ab83
Israel <Israel@github.com>
2018-03-20 20:33:17 +0200

    Camera

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -32,8 +32,23 @@ public class Camera {
     }
 
     //***************Operations*************************
-    public Ray constructRayThroughPixel(int Nx, int Ny,int i, int j, double screenDistance, double screenWidth, double screenHeight){
 
-        return null;
+    /**
+     *
+     * @param Nx - number of pixels in view plane in line X
+     * @param Ny - number of pixels in view plane in line Y
+     * @param i - describes the pixel we want in line x
+     * @param j - describes the pixel we want in line y
+     * @param screenDistance distance of the screen
+     * @param screenWidth - width of screen
+     * @param screenHeight - height of screen
+     * @return Ray from _P0 to pixel
+     */
+    public Ray constructRayThroughPixel(int Nx, int Ny,int i, int j, double screenDistance, double screenWidth, double screenHeight){
+        Point3D Pc=new Point3D(_p0.addVectorToPiont(_toward.multipliedbyScalar(screenDistance)));
+        double Rx=screenWidth/Nx;
+        double Ry=screenHeight/Ny;
+        Point3D Pij=Pc.addVectorToPiont((_right.multipliedbyScalar(Rx*(i-((Nx+1)/2)))).add(_up.multipliedbyScalar(Ry*((Ny+1)/2)-j)));
+        return new Ray(new Vector(Pij.get_x()-_p0.get_x(),Pij.get_y()-_p0.get_y(),Pij.get_z()-_p0.get_z()),_p0);
     }
 }
