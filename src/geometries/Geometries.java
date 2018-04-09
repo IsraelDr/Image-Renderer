cd7083ab370ad162ccbf9a29b4ce1a79010887f5
Israel <Israel@github.com>
2018-04-09 23:59:53 +0300

    Changes

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,17 @@
+package geometries;
+
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.ArrayList;
+
+public class Geometries extends Geometry {
+    public Vector getNormal(Point3D temp) {
+        return null;
+    }
+
+    public ArrayList<Point3D> findIntersections(Ray ray) {
+        return null;
+    }
+}
