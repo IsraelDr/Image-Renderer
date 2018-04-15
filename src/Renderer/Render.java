857b3148f7053ca6993ef813693d3f2141f68aa1
Israel <Israel@github.com>
2018-04-15 23:00:25 +0300

    Changes

---
+++ src/Renderer/Render.java
@@ -0,0 +1,16 @@
+package Renderer;
+
+
+import scene.Scene;
+
+import javax.imageio.ImageWriter;
+
+/**
+ * Render
+ */
+public class Render {
+    protected Scene _scene;
+    protected ImageWriter _imageWriter;
+
+
+}
