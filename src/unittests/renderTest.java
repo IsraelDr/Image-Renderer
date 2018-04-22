9869a2c774bbed3705452144a99c70bd443994e8
Israel <Israel@github.com>
2018-04-22 16:23:09 +0300

    Paint

--- src/unittests/renderTest.java
+++ src/unittests/renderTest.java
@@ -17,7 +17,10 @@ public class renderTest {
     @Test
     public void renderImage() {
         Scene scene=new Scene("testpic");
-        scene.addGeometry(new Triangle(new Point3D(1,-10,10),new Point3D(1,-10,0),new Point3D(1,0,10)));
+        scene.addGeometry(new Triangle(new Point3D(1,-100,100),new Point3D(1,-100,0),new Point3D(1,0,100)));
+        scene.addGeometry(new Triangle(new Point3D(1,-100,-100),new Point3D(1,-100,0),new Point3D(1,0,-100)));
+        scene.addGeometry(new Triangle(new Point3D(1,100,-100),new Point3D(1,100,0),new Point3D(1,0,-100)));
+        scene.addGeometry(new Triangle(new Point3D(1,100,0),new Point3D(1,100,100),new Point3D(1,0,100)));
         scene.addGeometry(new Sphere(new Point3D(1001,0,0),1000.8 ));
         ImageWriter imageWriter=new ImageWriter("testpic",501,501,501,501);
         Render render=new Render(scene,imageWriter);
