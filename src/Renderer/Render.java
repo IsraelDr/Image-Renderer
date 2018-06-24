e592637d682f4219dfa8f29ed1a3acf2d2698e12
Israel <Israel@github.com>
2018-06-24 13:11:07 +0300

    ()

--- src/Renderer/Render.java
+++ src/Renderer/Render.java
@@ -6,16 +6,12 @@ import elements.PointLight;
 import geometries.*;
 import primitives.*;
 import scene.Key;
-import scene.Raytrace;
 import scene.Scene;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Render
@@ -23,55 +19,47 @@ import java.util.concurrent.TimeUnit;
 public class Render {
     private Scene _scene;
     private ImageWriter _imageWriter;
-    private final int MAX_CALC_COLOR_LEVEL = 3;
-    private boolean ImprovedRenderer=true;
+    private final int MAX_CALC_COLOR_LEVEL = 7;
+    private boolean ImprovedRenderer=false;
     //****************Constructor****************//
     public Render(Scene scene, ImageWriter imageWriter) {
         this._scene = scene;
         this._imageWriter = new ImageWriter(imageWriter);
-        new Raytrace();
     }
 
     //********************Operations*************************//
-    public void renderImage()  throws InterruptedException {
-        final ThreadPoolExecutor executor = new ThreadPoolExecutor(12, 12, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+    public void renderImage() {
         int k;
-        //Plane firstplane=new Plane(_scene.getPoint3DbyKey(this._scene.MinKey()),_scene.getCamera().getToward(),new Color(0,0,0),new Material(0,0,0,0,0));
+        Plane firstplane=new Plane(_scene.getPoint3DbyKey(this._scene.MinKey()),_scene.getCamera().getToward(),new Color(0,0,0),new Material(0,0,0,0,0));
         for (int i = 1; i < _imageWriter.getNx(); i++) {
             for (int j = 1; j < _imageWriter.getNy(); j++) {
-                final int ii = i;
-                final int jj = j;
-                if (i == 500 && j >= 650) {
+                if (i == 500 && j >= 500) {
                     k = 5;
                     //this._imageWriter.writePixel(i, j, new java.awt.Color(255,0,0));
                     //continue;
                 }
-                executor.execute(() -> {
-                    Map<Geometry, List<Point3D>> intersectionPoints;
-                    Ray ray;
-                    if (ImprovedRenderer) {
-                        ray = _scene.getCamera().constructRayThroughPixel(_imageWriter.getNx(), _imageWriter.getNy(), ii, jj, _scene.getDistance(), _imageWriter.getWidth(), _imageWriter.getHeight());
-                        //List<Point3D> l=firstplane.findIntersections(ray);
+                Map<Geometry, List<Point3D>> intersectionPoints;
+                Ray ray;
+                if(ImprovedRenderer) {
+                    ray = _scene.getCamera().constructRayThroughPixel(_imageWriter.getNx(), _imageWriter.getNy(), i, j, _scene.getDistance(), _imageWriter.getWidth(), _imageWriter.getHeight());
+                    List<Point3D> l=firstplane.findIntersections(ray);
                     /*if(l.size()>0)
                         ray=new Ray(ray.get_vector(),l.get(0));*/
-                        intersectionPoints = getSceneRayIntersectionsImproved(ray);
-                    } else {
-                        ray = _scene.getCamera().constructRayThroughPixel(_imageWriter.getNx(), _imageWriter.getNy(), ii, jj, _scene.getDistance(), _imageWriter.getWidth(), _imageWriter.getHeight());
-                        intersectionPoints = getSceneRayIntersections(ray);
-                    }
-                    if (intersectionPoints.isEmpty())
-                        _imageWriter.writePixel(ii, jj, _scene.getBackgroundColor());
-                    else {
-                        Map<Geometry, Point3D> closestPoint = getClosestPoint(intersectionPoints);
-                        Map.Entry<Geometry, Point3D> entry = closestPoint.entrySet().iterator().next();
-                        this._imageWriter.writePixel(ii, jj, calcColor(entry.getKey(), entry.getValue(), ray).getColor());
-                    }
-                });
-
+                    intersectionPoints = getSceneRayIntersectionsImproved(ray);
+                }
+                else {
+                    ray = _scene.getCamera().constructRayThroughPixel(_imageWriter.getNx(), _imageWriter.getNy(), i, j, _scene.getDistance(), _imageWriter.getWidth(), _imageWriter.getHeight());
+                    intersectionPoints = getSceneRayIntersections(ray);
+                }
+                if (intersectionPoints.isEmpty())
+                    _imageWriter.writePixel(i, j, _scene.getBackgroundColor());
+                else {
+                    Map<Geometry, Point3D> closestPoint = getClosestPoint(intersectionPoints);
+                    Map.Entry<Geometry, Point3D> entry = closestPoint.entrySet().iterator().next();
+                    this._imageWriter.writePixel(i, j, calcColor(entry.getKey(), entry.getValue(), ray).getColor());
+                }
             }
         }
-        executor.shutdown();
-        executor.awaitTermination(5, TimeUnit.MINUTES);
     }
 
     /**
@@ -298,7 +286,7 @@ public class Render {
         int k=0;
         Map<Geometry, List<Point3D>> intersectionPoints = new HashMap<Geometry, List<Point3D>>();
         List<Point3D> geometryIntersectionPoints = new ArrayList<Point3D>();
-        List<Geometry> _geometries=getrelevantgeometries(ray,new ArrayList<>(),this._scene.MaxKey(),"front",1);
+        List<Geometry> _geometries=getrelevantgeometries(ray,new ArrayList<>(),this._scene.MaxKey());
         for (Geometry geometry : _geometries) {
             geometryIntersectionPoints = geometry.findIntersections(ray);
             if (!geometryIntersectionPoints.isEmpty())
@@ -315,176 +303,43 @@ public class Render {
         return intersectionPoints;
     }
 
-    private List<Geometry> getrelevantgeometries(Ray ray,List<Geometry> geometries , Key max,String planename,int k) {
-        Key mainKey=_scene.getKeybyPoint(ray.get_point().addVectorToPoint(ray.get_vector()));
-        Point3D tempp=_scene.getPoint3DbyKey(mainKey);
-        while(!(mainKey.isgreater(max)||k==0)) {
-            List<Geometry> current = this._scene.getCubemap().get(mainKey);
-            if (current != null) {
-                for (Geometry g : current) {
-                    geometries.add(g);
-                }
+    private List<Geometry> getrelevantgeometries(Ray ray,List<Geometry> geometries , Key max) {
+        Key mainKey=_scene.getKeybyPoint(ray.get_point());
+        if(mainKey.isgreater(max))
+            return geometries;
+        List<Geometry> current=this._scene.getCubemap().get(mainKey);
+        if(current!=null) {
+            for (Geometry g : current) {
+                geometries.add(g);
             }
-            Vector to = null, up = null, right = null;
-            double cx = 0, cy = 0, cz = 0;
-            Point3D cubep = null;
-            switch (planename) {
-                case "front":
-                    to = this._scene.getCamera().getToward();
-                    right = this._scene.getCamera().getRight();
-                    up = this._scene.getCamera().getUp();
-                    cx = this._scene.get_cubeDx();
-                    cy = this._scene.get_cubeDy();
-                    cz = this._scene.get_cubeDz();
-                    cubep = new Point3D(tempp);
-                    break;
-                case "up":
-                    to = this._scene.getCamera().getUp().multipliedbyScalar(-1);
-                    right = this._scene.getCamera().getRight();
-                    up = this._scene.getCamera().getToward();
-                    cx = this._scene.get_cubeDz();
-                    cy = this._scene.get_cubeDy();
-                    cz = this._scene.get_cubeDx();
-                    cubep = new Point3D(tempp.addVectorToPoint(this._scene.getCamera().getToward().multipliedbyScalar(cz)));
-                    break;
-                case "down":
-                    to = this._scene.getCamera().getUp();
-                    right = this._scene.getCamera().getRight();
-                    up = this._scene.getCamera().getToward().multipliedbyScalar(-1);
-                    cx = this._scene.get_cubeDz();
-                    cy = this._scene.get_cubeDy();
-                    cz = this._scene.get_cubeDx();
-                    cubep = new Point3D(tempp.addVectorToPoint(this._scene.getCamera().getUp().multipliedbyScalar(-cz)));
-                    break;
-                case "right":
-                    to = this._scene.getCamera().getRight().multipliedbyScalar(-1);
-                    right = this._scene.getCamera().getToward();
-                    up = this._scene.getCamera().getUp();
-                    cx = this._scene.get_cubeDy();
-                    cy = this._scene.get_cubeDx();
-                    cz = this._scene.get_cubeDz();
-                    cubep = new Point3D(tempp.addVectorToPoint(this._scene.getCamera().getRight().multipliedbyScalar(cx)));
-                    break;
-                case "left":
-                    to = this._scene.getCamera().getRight();
-                    right = this._scene.getCamera().getToward().multipliedbyScalar(-1);
-                    up = this._scene.getCamera().getUp();
-                    cx = this._scene.get_cubeDy();
-                    cy = this._scene.get_cubeDx();
-                    cz = this._scene.get_cubeDz();
-                    cubep = new Point3D(tempp.addVectorToPoint(this._scene.getCamera().getToward().multipliedbyScalar(cy)));
-                    break;
-                case "end":
-                    to = this._scene.getCamera().getToward().multipliedbyScalar(-1);
-                    right = this._scene.getCamera().getRight().multipliedbyScalar(-1);
-                    up = this._scene.getCamera().getUp();
-                    cx = this._scene.get_cubeDx();
-                    cy = this._scene.get_cubeDy();
-                    cz = this._scene.get_cubeDz();
-                    cubep = new Point3D(tempp.addVectorToPoint(this._scene.getCamera().getToward().multipliedbyScalar(cx)).addVectorToPoint(this._scene.getCamera().getToward().multipliedbyScalar(cy)));
-                    break;
-            }
-            double isup = ray.get_vector().ScalarProduct(up);
-            double isright = ray.get_vector().ScalarProduct(right);
-            double isto = ray.get_vector().ScalarProduct(to);
-            Point3D next = null;
-            String str = "";
-            List<Point3D> listpoints = new ArrayList<>();
-            if (isup == 0 && isright == 0) {
-                next = ray.get_point().addVectorToPoint(ray.get_vector().multipliedbyScalar(cx));
-                str = "end";
-            } else {
-                if (isright == 0) {
-                    if (isup > 0) {
-                        listpoints.add(new Plane(cubep, up, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        listpoints.add(new Plane(cubep.addVectorToPoint(to.multipliedbyScalar(cx)), to, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        if (ray.get_point().distance(listpoints.get(0)) < ray.get_point().distance(listpoints.get(1))) {
-                            next = listpoints.get(0);
-                            str = "up";
-                        } else {
-                            next = listpoints.get(1);
-                            str = "end";
-                        }
-
-                    } else {
-                        listpoints.add(new Plane(cubep.addVectorToPoint(to.multipliedbyScalar(cx)), to, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        listpoints.add(new Plane(cubep.addVectorToPoint(up.multipliedbyScalar(-cz)), up, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        if (ray.get_point().distance(listpoints.get(0)) < ray.get_point().distance(listpoints.get(1))) {
-                            next = listpoints.get(0);
-                            str = "end";
-                        } else {
-                            next = listpoints.get(1);
-                            str = "down";
-                        }
-                    }
-                } else if (isup == 0) {
-                    if (isright < 0) {
-                        listpoints.add(new Plane(cubep.addVectorToPoint(to.multipliedbyScalar(cx)), to, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        listpoints.add(new Plane(cubep, right, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        if (ray.get_point().distance(listpoints.get(0)) < ray.get_point().distance(listpoints.get(1))) {
-                            next = listpoints.get(0);
-                            str = "end";
-                        } else {
-                            next = listpoints.get(1);
-                            str = "left";
-                        }
-                    } else {
-                        listpoints.add(new Plane(cubep.addVectorToPoint(to.multipliedbyScalar(cx)), to, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        listpoints.add(new Plane(cubep.addVectorToPoint(right.multipliedbyScalar(cy)), right, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray).get(0));
-                        if (ray.get_point().distance(listpoints.get(0)) < ray.get_point().distance(listpoints.get(1))) {
-                            next = (listpoints.get(0));
-                            str = "end";
-                        } else {
-                            next = listpoints.get(1);
-                            str = "right";
-                        }
-                    }
-                } else {
-                    Map<Point3D, String> temp = new HashMap<>();
-                    List<Point3D> inters = new ArrayList<>();
-                    inters = new Plane(cubep.addVectorToPoint(to.multipliedbyScalar(cx)), to, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray);
-                    if (!inters.isEmpty())
-                        temp.put(inters.get(0), "end");
-                    inters.clear();
-
-                    if (isup > 0) {
-                        inters = new Plane(cubep, up, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray);
-                        if (!inters.isEmpty())
-                            temp.put(inters.get(0), "up");
-                        inters.clear();
-                    } else {
-                        inters = new Plane(cubep.addVectorToPoint(up.multipliedbyScalar(-cz)), up, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray);
-                        if (!inters.isEmpty())
-                            temp.put(inters.get(0), "down");
-                        inters.clear();
-                    }
-                    if (isright < 0) {
-                        inters = new Plane(cubep, right, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray);
-                        if (!inters.isEmpty())
-                            temp.put(inters.get(0), "left");
-                    } else {
-                        inters = new Plane(cubep.addVectorToPoint(right.multipliedbyScalar(cy)), right, Color.BLACK, new Material(0, 0, 0, 0, 0)).findIntersections(ray);
-                        if (!inters.isEmpty())
-                            temp.put(inters.get(0), "right");
-
-                    }
-                    double dist = Double.MAX_VALUE;
-                    for (Map.Entry<Point3D, String> p : temp.entrySet()) {
-                        double m = p.getKey().distance(ray.get_point());
-                        if (m < dist) {
-                            dist = m;
-                            next = p.getKey();
-                            str = p.getValue();
-                        }
-                    }
-                }
-            }
-            ray=new Ray(ray.get_vector(),next);
-            planename=Raytrace.arr.get(planename).get(str);
-            k--;
         }
-        return geometries;
-        //return getrelevantgeometries(new Ray(,geometries,max,,k--);
+        Plane top=new Plane(this._scene.getPoint3DbyKey(mainKey),_scene.getCamera().getUp(),Color.BLACK,new Material(0,0,0,0,0));
+        Plane left=new Plane(this._scene.getPoint3DbyKey(mainKey),_scene.getCamera().getRight(),Color.BLACK,new Material(0,0,0,0,0));
+        Plane right=new Plane(this._scene.getPoint3DbyKey(new Key(mainKey.getX()+1,mainKey.getY()+1,mainKey.getZ()-1)),_scene.getCamera().getRight(),Color.BLACK,new Material(0,0,0,0,0));
+        Plane down=new Plane(this._scene.getPoint3DbyKey(new Key(mainKey.getX()+1,mainKey.getY()+1,mainKey.getZ()-1)),_scene.getCamera().getUp(),Color.BLACK,new Material(0,0,0,0,0));
+        Plane forward=new Plane(this._scene.getPoint3DbyKey(new Key(mainKey.getX()+1,mainKey.getY()+1,mainKey.getZ()-1)),_scene.getCamera().getToward(),Color.BLACK,new Material(0,0,0,0,0));
+        List<Point3D> listpoints=new ArrayList<>();
+        if(top.findIntersections(ray).size()>0)
+            listpoints.add(top.findIntersections(ray).get(0));
+        if(left.findIntersections(ray).size()>0)
+            listpoints.add(left.findIntersections(ray).get(0));
+        if(right.findIntersections(ray).size()>0)
+            listpoints.add(right.findIntersections(ray).get(0));
+        if(down.findIntersections(ray).size()>0)
+            listpoints.add(down.findIntersections(ray).get(0));
+        if(forward.findIntersections(ray).size()>0)
+            listpoints.add(forward.findIntersections(ray).get(0));
+        double dist=Double.MAX_VALUE;
+        if(listpoints.size()==0)
+            return geometries;
+        Point3D min=null;
+        for (Point3D p:listpoints) {
+            if(p.distance(ray.get_point())<dist){
+                dist=p.distance(ray.get_point());
+                min=new Point3D(p);
+            }
+        }
+        return getrelevantgeometries(new Ray(ray.get_vector(),min),geometries,max);
     }
 
     /**
